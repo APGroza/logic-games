e3ddddf1947bdd84ce43fe88797081c6c775509c
APGroza <62267054+APGroza@users.noreply.github.com>
2021-11-11 13:05:12 +0200

    Add files via upload

--- ch_02_strange_numbers/run.sh
+++ ch_02_strange_numbers/run.sh
@@ -1,14 +1,43 @@
+#Chapter 3. Strange numbers
+#0. Intro
 #mace4 -f romeojulieta.in | interpformat standard > romeojulieta.out
+
+#1. Adding their cubes
 #mace4 -f addingcubes.in | interpformat standard > adddingcubes.out
+
+#2. Multiplication
 #mace4 -f multiplication.in | interpformat standard > multiplication.out
+
+#3. The arithmetical cabby
 #mace4 -f arithmeticalcabby.in | interpformat standard > arithmeticalcabby.out
+
+#4. The ten barrels
 #mace4 -f tenbarrels.in | interpformat standard > tenbarrels.out
+
+#5. Find the triangle area
 #mace4 -f trianglearea.in | interpformat standard > trianglearea.out
+
+#6. A digital difficulty
 #mace4 -f digitaldifficulty.in | interpformat standard > digitaldifficulty.out
+
+#7. The archery match
 #mace4 -f archerymatch.in | interpformat standard > archerymatch.out
+
+#8. Target practice
 #mace4 -f targetpractice.in | interpformat standard > targetpractice.out
+
+#9. The nine barrels
 #mace4 -f ninebarrels.in | interpformat standard > ninebarrels.out
+
+#10. Upside-down year
 #mace4 -f upsideyearb.in | interpformat standard > upsideyearb.out
+
+#11. A twisted number
 #mace4 -f rotate.in | interpformat standard > rotate.out
-mace4 -f unusualnumber.in | interpformat standard > unusualnumber.out
+
+#12. Unusual number
+#mace4 -f unusualnumber.in | interpformat standard > unusualnumber.out
+
+
+
 
